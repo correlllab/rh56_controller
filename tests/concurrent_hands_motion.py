d2eba5cf5121ebc3de66c5fc354dab51a9543cf1
William Xie <williamxie00@gmail.com>
2025-08-05 13:33:35 -0600

    test foldeR

---
+++ tests/concurrent_hands_motion.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+sys.path.append("../")
+from rh56_controller.rh56_hand import RH56Hand
+
+# test moving both hands concurrently
+if __name__ == "__main__":
+    # Initialize the right hand
+    right_hand = RH56Hand("/dev/ttyUSB0", 1)
+    left_hand = RH56Hand("/dev/ttyUSB0", 2)
+
+    # Move both hands concurrently
+    close = [0] * 6
+    open = [1000] * 6
+
+    # create user input to accept either 'c' or 'close' for close or 'o' for open
+    user_input = input("Enter 'c' for close or 'o' for open: ")
+
+    if user_input in ['c', 'close']:
+        right_hand.move_to_position(close)
+        left_hand.move_to_position(close)
+    elif user_input in ['o', 'open']:
+        right_hand.move_to_position(open)
+        left_hand.move_to_position(open)
+    else:
+        print("Invalid input. Please enter 'c' or 'o'.")
